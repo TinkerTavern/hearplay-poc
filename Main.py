3a2a1539148623b2bd9888161c1c2852cb0adeb1
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2020-08-16 15:01:17 +0100

    Update Main.py
    Now tests all 5 levels!
    Note some mics playback weirdly

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import time
+
 from NotePlayer import NotePlayer
 from NotePlayback import NotePlayback
 from NoteRecorder import NoteRecorder
@@ -5,3 +7,33 @@ from NoteRecorder import NoteRecorder
 note = NotePlayer()
 record = NoteRecorder()
 player = NotePlayback()
+
+note.round1()
+record.record()
+player.playBack()
+note.round1()
+time.sleep(2)
+
+note.round2()
+record.record()
+player.playBack()
+note.round2()
+time.sleep(2)
+
+note.round3()
+record.record()
+player.playBack()
+note.round3()
+time.sleep(2)
+
+note.round4()
+record.record()
+player.playBack()
+note.round4()
+time.sleep(2)
+
+note.round5()
+record.record()
+player.playBack()
+note.round5()
+time.sleep(2)
